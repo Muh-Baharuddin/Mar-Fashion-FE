ccc1f58edb9662ecc7e11ba6e32c1abdd51d02a9
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-01-27 13:16:24 +0700

    setting next config to access Api Endpoint from env file

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
 }
 
 module.exports = nextConfig
