8a1616c4e6cfc75e01230f3eabd3f70cb1cff33b
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-02-06 14:18:56 +0700

    create toko page for guess

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,24 +1,44 @@
+import { Button, Col, Row } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Link from "next/link";
 
-function KitchenSinkExample() {
+function CardComp() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-    </Card>
+    <div>
+      <Row className="my-4">
+      {[1,2,3,4,5,6,7].map(e => {
+        return (
+          <Col>
+            <Card style={{ width: '20rem'}}>
+              <Card.Img variant="top" src="https://hinyong.com/wp-content/uploads/2019/05/img_5696.jpg" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              </ListGroup>
+              <Button variant="danger">
+                  <Link
+                    href="/toko"
+                    style={{ textDecoration: "none", color: "white"}}
+                  >
+                    Go somewhere
+                  </Link>
+                </Button>
+            </Card>
+          </Col>
+        )
+      })}
+      </Row>
+    </div>
   );
 }
 
-export default KitchenSinkExample;
+export default CardComp;
--- src/pages/toko.tsx
+++ src/pages/toko.tsx
@@ -1,190 +1,19 @@
 import { Button, Card, Container, Row, Col } from "react-bootstrap";
 import Link from "next/link";
-import { AdminLayout } from '@layouts/AdminLayout/index'
+import NavbarComp from "src/components/Navbar/NavbarComp";
+import React from "react";
+import CardComp from "src/components/Cards/Cards";
 
 const TokoPage = () => {
+  const [login, setLogin] = React.useState(false);
+  
   return (
-    <AdminLayout>
+    <div>
+      <NavbarComp isLogin={login}/>
       <Container>
-        <Row className="my-4">
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Baju</Card.Title>
-                <Card.Text>
-                  Terdapat berbagai macam baju dengan kualitas top dengan harga
-                  termurah
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Celana</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the content.
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the content.
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="my-4">
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Baju</Card.Title>
-                <Card.Text>
-                  Terdapat berbagai macam baju dengan kualitas top dengan harga
-                  termurah
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Celana</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the content.
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="celana"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the content.
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="my-4">
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Baju</Card.Title>
-                <Card.Text>
-                  Terdapat berbagai macam baju dengan kualitas top dengan harga
-                  termurah
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Celana</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the content.
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "100%" }}>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the content.
-                </Card.Text>
-                <Button variant="danger">
-                  <Link
-                    href="/daftarBarang/baju"
-                    style={{ textDecoration: "none", color: "white" }}
-                  >
-                    Go somewhere
-                  </Link>
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <CardComp />
       </Container>
-    </AdminLayout>
+    </div>
   )
 }
 
