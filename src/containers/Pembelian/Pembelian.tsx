ad98acd75da6822c3cb75b5f70d533b99b18f880
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-02-13 20:18:56 +0700

    merge pembelian container file and table pembelian, create pagination

--- src/containers/Pembelian/Pembelian.tsx
+++ src/containers/Pembelian/Pembelian.tsx
@@ -1,19 +1,151 @@
 import { AdminLayout } from '@layouts/AdminLayout'
-import { useState } from 'react'
-import TablePembelian from './Table/TablePembelian'
+import { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie'
+import axios from 'axios'
+import AddModalPembelian from './AddModalPembelian'
+import EditModalPembelian from './EditModalPembelian'
+import Pagination from 'react-paginate'
+
+interface Data {
+  id: string
+  tanggal: string
+  supplier: string
+  barang: string
+  biaya: number
+}
 
 export const DataPembelian = () => {
+  const [cookies] = useCookies(['token', 'user'])
+  const [data, setData] = useState<Data[]>([])
   const [showAdd, setShowAdd] = useState(false)
   const [showEdit, setShowEdit] = useState(false)
+  const [editId, setEditId] = useState('')
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [perPage, setPerPage] = useState(5);
+  const [offset, setOffset] = useState(0);
+
+  const handleShowAdd = () => setShowAdd(true)
+  const handleCloseAdd = () => setShowAdd(false)
+
+  const handleShowEdit = (id: string) => {
+    setEditId(id)
+    setShowEdit(true)
+  }
+
+  const handleCloseEdit = () => {
+    setEditId('')
+    setShowEdit(false)
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/nota-pembelian', config).then((response) => {
+      setData(response.data)
+    })
+  }, [])
+
+  let token = cookies.token
+  let config = {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  }
+
+  const handleDelete = (id: string) => {
+    axios
+      .delete('http://localhost:4000/nota-pembelian/' + id, config)
+      .then((response) => {
+        console.log('ini nilai respon', response)
+        alert(response.data.message)
+        window.location.reload()
+      })
+  }
+
+  const handlePageClick = (e: { selected: number }) => {
+    const selectedPage = e.selected;
+    setCurrentPage(selectedPage);
+    setOffset(selectedPage * perPage);
+  }
+
   return (
     <AdminLayout>
       <div className="container">
-        <TablePembelian
-          showAdd={showAdd}
-          showEdit={showEdit}
-          setShowAdd={setShowAdd}
-          setShowEdit={setShowEdit}
-        />
+        <div className="card">
+          <div className="card-header">
+            <AddModalPembelian showAdd={showAdd} handleCloseAdd={handleCloseAdd} />
+            <EditModalPembelian
+              showEdit={showEdit}
+              editId={editId}
+              handleCloseEdit={handleCloseEdit}
+            />
+            {cookies.user && (
+              <button onClick={handleShowAdd} className="btn btn-primary">
+                <i className="bi bi-plus-square"></i>
+              </button>
+            )}
+          </div>
+          <div className="card-body">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Tanggal</th>
+                  <th>Barang</th>
+                  <th>Supplier</th>
+                  <th>Total Biaya</th>
+                  {cookies.user && <th>Action</th>}
+                </tr>
+              </thead>
+              <tbody>
+                {data &&
+                  Object.values(data)
+                  .slice(offset, offset + perPage)
+                  .map((d, index) => {
+                    return (
+                      <tr key={d.id}>
+                        <td>{index + 1 + offset}</td>
+                        <td>{d.tanggal}</td>
+                        <td>{d.supplier}</td>
+                        <td>{d.barang}</td>
+                        <td>{d.biaya}</td>
+                        {cookies.user && (
+                        <td>
+                          <button
+                            onClick={() => handleShowEdit(d.id)}
+                            className="btn btn-primary ms-3"
+                          >
+                            <i className="bi bi-pencil-square"></i>
+                          </button>
+                          <button
+                            onClick={() => handleDelete(d.id)}
+                            className="btn btn-danger ms-3"
+                          >
+                            <i className="bi bi-trash3-fill"></i>
+                          </button>
+                        </td>
+                        )}
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <Pagination
+              previousLabel={'previous'}
+              nextLabel={'next'}
+              pageCount={Math.ceil(data.length / perPage)}
+              marginPagesDisplayed={0}
+              pageRangeDisplayed={3}
+              onPageChange={handlePageClick}
+              containerClassName="pagination-container" 
+              activeClassName="selected"
+              disabledClassName="disabled"
+              pageLinkClassName={'pagination-item'}
+            />
+          </div>
+        </div>
       </div>
     </AdminLayout>
   )
