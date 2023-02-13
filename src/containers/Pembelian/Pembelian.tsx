ff44fbf86f20854ab1cfac287c5f8e42cceeb283
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-02-13 14:10:06 +0700

    delete unused code

--- src/containers/Pembelian/Pembelian.tsx
+++ src/containers/Pembelian/Pembelian.tsx
@@ -1,37 +1,20 @@
 import { AdminLayout } from '@layouts/AdminLayout'
-import { KaryawanLayout } from '@layouts/KaryawanLayout'
 import { useState } from 'react'
-import { useCookies } from 'react-cookie'
 import TablePembelian from './Table/TablePembelian'
 
 export const DataPembelian = () => {
-  const [cookies] = useCookies(['user'])
   const [showAdd, setShowAdd] = useState(false)
   const [showEdit, setShowEdit] = useState(false)
   return (
-    <div>
-    { cookies.user?.role == "ADMIN" ? 
-      <AdminLayout>
-        <div className="container">
-          <TablePembelian
-            showAdd={showAdd}
-            showEdit={showEdit}
-            setShowAdd={setShowAdd}
-            setShowEdit={setShowEdit}
-          />
-        </div>
-      </AdminLayout> :
-      <KaryawanLayout>
-        <div className="container">
-          <TablePembelian
-            showAdd={showAdd}
-            showEdit={showEdit}
-            setShowAdd={setShowAdd}
-            setShowEdit={setShowEdit}
-          />
-        </div>
-      </KaryawanLayout>
-    }
-    </div>
+    <AdminLayout>
+      <div className="container">
+        <TablePembelian
+          showAdd={showAdd}
+          showEdit={showEdit}
+          setShowAdd={setShowAdd}
+          setShowEdit={setShowEdit}
+        />
+      </div>
+    </AdminLayout>
   )
 }
--- src/models/barang.ts
+++
@@ -1,8 +0,0 @@
-export interface Barang {
-  id: number;
-  merek: string;
-  size: string;
-  warna: string;
-  stok: number;
-  harga: number;
-}
