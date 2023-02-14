b0a96b8d0a3312a979a1bbbd44091bfcb19a6756
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-02-14 14:17:23 +0700

    delete guess layout in login container and put guess layout in login page

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,38 +1,33 @@
 import { Col, Container, Row } from 'react-bootstrap'
 import Image from 'next/image'
 import { LoginForm } from './LoginForm'
-import { MarFashionContext } from 'src/context/MarFashionProvider'
-import { useContext, useState } from 'react'
-import { GuessLayout } from '@layouts/GuessLayout'
 
 export const Login = () => {
   return (
-    <GuessLayout>
-      <div className="bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent">
-        <Container>
-          <Row className="justify-content-center align-items-center px-3">
-            <Col lg={8}>
-              <Row>
-                <Col md={7} className="bg-white border p-5">
-                  <div className="">
-                    <h1>Login</h1>
-                    <p className="text-black-50">Silahkan masukkan akun anda</p>
-                      <LoginForm />
-                  </div>
-                </Col>
-                <Col
-                  md={5}
-                  className="bg-white border p-5 text-white d-flex align-items-center justify-content-center p-5"
-                >
-                  <div className="text-center">
-                    <Image src="/MarFashion.png" alt="Mar Fashion Logo" width={250} height={250} />
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </GuessLayout>
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent">
+      <Container>
+        <Row className="justify-content-center align-items-center px-3">
+          <Col lg={8}>
+            <Row>
+              <Col md={7} className="bg-white border p-5">
+                <div className="">
+                  <h1>Login</h1>
+                  <p className="text-black-50">Silahkan masukkan akun anda</p>
+                    <LoginForm />
+                </div>
+              </Col>
+              <Col
+                md={5}
+                className="bg-white border p-5 text-white d-flex align-items-center justify-content-center p-5"
+              >
+                <div className="text-center">
+                  <Image src="/MarFashion.png" alt="Mar Fashion Logo" width={250} height={250} />
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
