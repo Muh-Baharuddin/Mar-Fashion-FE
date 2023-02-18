880a1d65326a601cc306fc4f7f03ec6f03227e3b
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-02-18 12:12:05 +0700

    create confirm delete with react-confirm-alert for supplier

--- src/containers/Supplier/Components/DeleteComp.tsx
+++ src/containers/Supplier/Components/DeleteComp.tsx
@@ -1,4 +1,6 @@
 import axios from "axios"
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 type Props = {
   id: string;
@@ -6,23 +8,34 @@ type Props = {
 
 const DeleteComp = ({ id }: Props) => {
   const handleDelete = () => {
-    const confirmDelete = window.confirm('Apakah Anda yakin ingin menghapus data ini?');
-    if (confirmDelete) {
-      axios
-        .delete(`${process.env.API_ENDPOINT}supplier/` + id)
-        .then((response) => {
-          alert(response.data.message)
-          window.location.reload()
-        })
-    }
-  }
+    confirmAlert({
+      title: 'Konfirmasi',
+      message: 'Apakah Anda yakin ingin menghapus data ini?',
+      buttons: [
+        {
+          label: 'Ya',
+          onClick: () => {
+            axios
+            .delete(`${process.env.API_ENDPOINT}supplier/` + id)
+            .then((response) => {
+              alert(response.data.message)
+              window.location.reload()
+            })
+            .catch(() => {
+              alert("Maaf sepertinya terjadi kesalahan pada server. Mohon coba kembali dalam beberapa saat.");
+            });
+          },
+        },
+        {
+          label: 'Tidak',
+        }
+      ],
+    });
+  };
   return (
     <div>
-      <button
-        onClick={() => handleDelete()}
-        className="btn btn-danger ms-3"
-      >
-      <i className="bi bi-trash3-fill"></i>
+      <button onClick={() => handleDelete()} className="btn btn-danger ms-3">
+        <i className="bi bi-trash3-fill"></i>
       </button>
     </div>
   )
