bfc06b1ea19c66b00becfea1458f2fe0ace81b2e
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-03-08 15:07:48 +0700

    fix employee table

--- src/containers/Employee/Table/TableEmployee.tsx
+++ src/containers/Employee/Table/TableEmployee.tsx
@@ -1,88 +1,33 @@
-import {
-  Dispatch,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react'
-import { useMarContext } from 'src/context/MarFashionProvider'
-import axios from 'axios'
-import Pagination from 'react-paginate'
+import { useEmployeeContext } from '../Employee';
+import { getEmployees } from 'services/employee';
+import { CSSProperties } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import AddModalKaryawan from '../Modal/AddModalEmployee'
-import EditModalKaryawan from '../Modal/EditModalEmployee'
 
-interface Data {
-  id: string
-  nama: string
-  alamat: string
-  nomor_telepon: string
-}
+const TableEmployee = () => {
+  const { queryParams, setQueryParams } = useEmployeeContext()
+  const { data, error, isLoading } = getEmployees(queryParams);
 
-type handleShowType = {
-  showAdd: boolean
-  showEdit: boolean
-  setShowAdd: Dispatch<SetStateAction<boolean>>
-  setShowEdit: Dispatch<SetStateAction<boolean>>
-}
+  const handleSortBy = (column: string) => {
+    let newOrderType = 'ASC';
+    if (column === queryParams.orderBy && queryParams.orderType === 'ASC') {
+      newOrderType = 'DESC';
+    }
+    setQueryParams({
+      ...queryParams,
+      orderBy: column,
+      orderType: newOrderType,
+    });
+  };
 
-const TableEmployee = (props: handleShowType) => {
-  const { showAdd, showEdit, setShowAdd, setShowEdit } = props
-  const { user } = useMarContext()
-  const [data, setData] = useState<Data[]>([])
-  const [editId, setEditId] = useState('')
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
 
-  const [currentPage, setCurrentPage] = useState(0);
-  const [perPage, setPerPage] = useState(5);
-  const [offset, setOffset] = useState(0);
-
-  const handlePageClick = (e: { selected: number }) => {
-    const selectedPage = e.selected;
-    setCurrentPage(selectedPage);
-    setOffset(selectedPage * perPage);
-  }
-
-  const handleShowAdd = () => setShowAdd(true)
-  const handleCloseAdd = () => setShowAdd(false)
-
-  const handleShowEdit = (id: string) => {
-    setEditId(id)
-    setShowEdit(true)
-  }
-
-  const handleCloseEdit = () => {
-    setEditId('')
-    setShowEdit(false)
-  }
-
-  useEffect(() => {
-    axios.get(`${process.env.API_ENDPOINT}karyawan`).then((response) => {
-      setData(response.data)
-    })
-  }, [])
-
-  const handleDelete = (id: string) => {
-    axios
-      .delete(`${process.env.API_ENDPOINT}karyawan/` + id)
-      .then((response) => {
-        alert(response.data.message)
-        window.location.reload()
-      })
-  }
   return (
-    <div className="card">
-      <div className="card-header">
-        <AddModalKaryawan showAdd={showAdd} handleCloseAdd={handleCloseAdd} />
-        <EditModalKaryawan
-          showEdit={showEdit}
-          editId={editId}
-          handleCloseEdit={handleCloseEdit}
-        />
-        { user && (
-          <button onClick={handleShowAdd} className="btn btn-primary">
-            <i className="bi bi-plus-square"></i>
-          </button>
-        )}
-      </div>
+    <>
       <div className="card-body">
         <table className="table table-bordered">
           <thead>
@@ -91,55 +36,42 @@ const TableEmployee = (props: handleShowType) => {
               <th>Nama</th>
               <th>Alamat</th>
               <th>Nomor Telepon</th>
-              { user && <th>Action</th>}
+              <th>Tanggal Masuk</th>
+              <th>Tanggal Keluar</th>
+              <th>Total Tabungan</th>
             </tr>
           </thead>
           <tbody>
-            {data &&
-              Object.values(data)
-              .slice(offset, offset + perPage)
-              .map((d, index) => {
+            { isLoading ? (
+              <tr>
+                <td colSpan={5} style={{ textAlign:'center' }}>
+                  <BeatLoader 
+                    color='silver'
+                    cssOverride={override}
+                    size={15}
+                  />
+                </td>
+              </tr>
+            ) : data && (
+              Object.values(data.data).map((d, index) => {
                 return (
                   <tr key={d.id}>
-                    <td>{index + 1 + offset}</td>
-                    <td>{d.nama}</td>
-                    <td>{d.alamat}</td>
-                    <td>{d.nomor_telepon}</td>
-                    { user && (
-                      <td>
-                        <button
-                          onClick={() => handleShowEdit(d.id)}
-                          className="btn btn-primary ms-3"
-                        >
-                          <i className="bi bi-pencil-square"></i>
-                        </button>
-                        <button
-                          onClick={() => handleDelete(d.id)}
-                          className="btn btn-danger ms-3"
-                        >
-                          <i className="bi bi-trash3-fill"></i>
-                        </button>
-                      </td>
-                    )}
+                    <td>
+                      {(queryParams.page-1) * queryParams.limit + index + 1}
+                    </td>
+                    <td>{d.name}</td>
+                    <td>{d.address}</td>
+                    <td>{d.phone_number}</td>
+                    <td>{d.entry_date.toLocaleDateString()}</td>
+                    <td>{d.exit_date.toLocaleDateString()}</td>
+                    <td>{d.total_saving}</td>
                   </tr>
                 )
-              })}
+              }))}
           </tbody>
         </table>
-        <Pagination
-          previousLabel={'previous'}
-          nextLabel={'next'}
-          pageCount={Math.ceil(data.length / perPage)}
-          marginPagesDisplayed={0}
-          pageRangeDisplayed={3}
-          onPageChange={handlePageClick}
-          containerClassName="pagination-container" 
-          activeClassName="selected"
-          disabledClassName="disabled"
-          pageLinkClassName={'pagination-item'}
-        />
       </div>
-    </div>
+    </>
   )
 }
 
