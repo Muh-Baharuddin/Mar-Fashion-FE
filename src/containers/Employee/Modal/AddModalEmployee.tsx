353d6dbbdcdef910bdc422ee70eb50c0bced1012
Muh-Baharuddin <4519210022@univpancasila.ac.id>
2023-05-21 16:59:26 +0700

    delete unused code

--- src/containers/Employee/Modal/AddModalEmployee.tsx
+++ src/containers/Employee/Modal/AddModalEmployee.tsx
@@ -20,7 +20,6 @@ function AddModalEmployee(props: handleShowType) {
   const { formData } = useFormContext<Employee>()
 
   const handleAdd = (data: AddEmployee) => {
-    console.log("asdasd")
     postEmployee(data).then(() => {
       toast.success('Data berhasil ditambahkan');
       tableData.control.refresh();
